df85fac61e2f88422f4d810d3598a5be83e4bd29
Tony Swearingen <unlimited1974@aol.com>
2021-12-28 13:16:06 -0600

    code pushed

--- index.js
+++ index.js
@@ -5,8 +5,9 @@ function writeCards(names, events) {
 for (let i = 0; i < names.length; i++) {
     console.log(`Thank you,${names[i]}, for the wonderful ${events} gift!`);
     }
-return writeCards;
+
 }
+writeCards(names, events);
 
 
 function countDown() {
