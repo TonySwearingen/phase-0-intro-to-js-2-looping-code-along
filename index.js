34ead762ede4533a3d0491029aa712f80e2760a7
Tony Swearingen <unlimited1974@aol.com>
2021-12-28 13:06:32 -0600

    code pushed

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+const names = ["Guadalupe", "Ollie", "Aki"];
+const events = ["surprise"]
+
+function writeCards(names, events) {
+for (let i = 0; i < names.length; i++) {
+    console.log(`Thank you,${names[i]}, for the wonderful ${events} gift!`);
+    }
+return writeCards;
+}
+
+
+function countDown() {
+    let countDown = 10;
+    while ( countDown > -1) {
+        console.log(countDown--);
+    }
+} 
